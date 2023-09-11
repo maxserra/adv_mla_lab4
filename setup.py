95723e61c9008da610e7ff30a0822ff3004a10a0
Max Serra <mserralasierra@gmail.com>
2023-09-11 15:38:00 +1000

    inital commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Lab 4',
+    author='Max Serra',
+    license='MIT',
+)
